9800b610a026be960cf063b245ddddd3ae5bbf89
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-15 22:42:08 +0800

    Hilary: Done Employees POST Interface

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,15 +11,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    private List<Employee> employees = new ArrayList<>();
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getAllEmployee(){
-        List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(1,"Hilary",23,"Female"));
         employees.add(new Employee(2,"Jay",30,"Male"));
         employees.add(new Employee(3,"Candy",23,"Female"));
         employees.add(new Employee(4,"Tommy",26,"Male"));
         return employees;
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createNewEmployee(@RequestBody Employee employee){
+        employees.add(employee);
+        return employee;
+    }
+
+
+
 
 }
