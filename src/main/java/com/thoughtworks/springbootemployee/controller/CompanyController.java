590fadea5534c2d7d70e42b1ba3a0cde1170befd
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-16 02:52:13 +0800

    Hilary: reformat the line

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -17,13 +17,13 @@ public class CompanyController {
 
     @GetMapping
     public List<Company> getAllCompanies() {
-        employees.add(new Employee(1, "Hilary", 23, "female",10000));
-        employees.add(new Employee(2, "Jay", 30, "male",10000));
-        employees.add(new Employee(3, "Candy", 23, "female",10000));
-        employees.add(new Employee(4, "Tommy", 26, "male",10000));
+        employees.add(new Employee(1, "Hilary", 23, "female", 10000));
+        employees.add(new Employee(2, "Jay", 30, "male", 10000));
+        employees.add(new Employee(3, "Candy", 23, "female", 10000));
+        employees.add(new Employee(4, "Tommy", 26, "male", 10000));
 
-        companies.add(new Company(1,"alibaba",200, this.employees.subList(0, 2)));
-        companies.add(new Company(2,"chocolate factory",50, this.employees.subList(2, 4)));
+        companies.add(new Company(1, "alibaba", 200, this.employees.subList(0, 2)));
+        companies.add(new Company(2, "chocolate factory", 50, this.employees.subList(2, 4)));
         return companies;
     }
 
