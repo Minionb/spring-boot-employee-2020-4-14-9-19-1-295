602c57329245dbd0f9ad8dfa45b063aecee1004f
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-16 19:12:34 +0800

    Done should_return_all_companies test case

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -15,16 +15,19 @@ public class CompanyController {
     private List<Company> companies = new ArrayList<>();
     private List<Employee> employees = new ArrayList<>();
 
+    public CompanyController(){
+        this.employees.add(new Employee(1, "Hilary", 23, "female", 10000));
+        this.employees.add(new Employee(2, "Jay", 30, "male", 10000));
+        this.employees.add(new Employee(3, "Candy", 23, "female", 10000));
+        this.employees.add(new Employee(4, "Tommy", 26, "male", 10000));
+
+        this.companies.add(new Company(1, "Alibaba", 200, this.employees.subList(0, 2)));
+        this.companies.add(new Company(2, "Chocolate Factory", 50, this.employees.subList(2, 4)));
+
+    }
     @GetMapping
     public List<Company> getAllCompanies() {
-        employees.add(new Employee(1, "Hilary", 23, "female", 10000));
-        employees.add(new Employee(2, "Jay", 30, "male", 10000));
-        employees.add(new Employee(3, "Candy", 23, "female", 10000));
-        employees.add(new Employee(4, "Tommy", 26, "male", 10000));
-
-        companies.add(new Company(1, "alibaba", 200, this.employees.subList(0, 2)));
-        companies.add(new Company(2, "chocolate factory", 50, this.employees.subList(2, 4)));
-        return companies;
+        return this.companies;
     }
 
     @GetMapping(path = "/{companyId}")
--- src/test/java/com/thoughtworks/springbootemployee/controller/CompanyControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/CompanyControllerTest.java
@@ -23,9 +23,33 @@ import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 @SpringBootTest
 public class CompanyControllerTest {
     @Autowired
-    private EmployeeController companyController;
+    private CompanyController companyController;
     private List<Company> companies = new ArrayList<>();
     private List<Employee> employees = new ArrayList<>();
 
+    @Before
+    public void setUp() throws Exception{
+        RestAssuredMockMvc.standaloneSetup(companyController);
+    }
+
+    @Test
+    public void should_return_all_companies(){
+        MockMvcResponse response = given().contentType(ContentType.JSON)
+                .when()
+                .get("/companies");
+
+        Assert.assertEquals(200, response.getStatusCode());
+
+        List<Company> companies = response.getBody().as(new TypeRef<List<Company>>() {
+            @Override
+            public Type getType() {
+                return super.getType();
+            }
+        });
+
+        Assert.assertEquals(2, companies.size());
+        Assert.assertEquals("Alibaba", companies.get(0).getCompanyName());
+        Assert.assertEquals("Chocolate Factory", companies.get(1).getCompanyName());
+    }
 
 }
