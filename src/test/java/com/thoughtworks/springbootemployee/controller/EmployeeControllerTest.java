cfc35d233260ea10e21144c731e9ba73d69ab287
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-16 16:11:26 +0800

    Done should_update_employee_information_by_id test case

--- src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
@@ -123,4 +123,18 @@ public class EmployeeControllerTest {
         Assert.assertEquals("Hilary", employee.getName());
 
     }
+
+    @Test
+    public void should_update_employee_information_by_id(){
+        Employee newEmployee = new Employee(1,"Kathy",26,"female",10000);
+        MockMvcResponse response = given().contentType(ContentType.JSON)
+                .body(newEmployee)
+                .when()
+                .put("/employees/1");
+
+        Assert.assertEquals(200, response.getStatusCode());
+        Employee employee = response.getBody().as(Employee.class);
+        Assert.assertEquals(1, employee.getId());
+        Assert.assertEquals("Kathy", employee.getName());
+    }
 }
