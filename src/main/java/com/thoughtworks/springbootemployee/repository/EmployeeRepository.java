0b7a747a4904ad2e31792655587d8958b0a1e661
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-19 12:31:31 +0800

    Hilary: Done should_return_employee_when_insert_id

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -2,11 +2,19 @@ package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findAllByGender(String gender);
+
+    @Modifying
+    @Query(value = "ALTER TABLE EMPLOYEE ALTER COLUMN ID RESTART WITH 1",nativeQuery = true)
+    void resetAutoIncrement();
 }
--- src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
@@ -14,7 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.mapper.TypeRef;
+
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
@@ -29,30 +32,39 @@ public class EmployeeControllerTest {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @Before
-    public void setUp() throws Exception{
-        RestAssuredMockMvc.standaloneSetup(employeeController);
-//        employeeRepository.setEmployees(new ArrayList<>(Arrays.asList(
-//                new Employee(1, "Hilary", 23, "female", 10000),
-//                new Employee(2, "Jay", 30, "male", 10000),
-//                new Employee(3, "Candy", 23, "female", 10000),
-//                new Employee(4, "Tommy", 26, "male", 10000)
-//        )));
+    List<Employee> employeesList = new ArrayList<>();
 
+    @Before
+    public void setUp() throws Exception {
+        RestAssuredMockMvc.standaloneSetup(employeeController);
+//        employeesList = new ArrayList<>(Arrays.asList(
+//                new Employee(1, "Hilary", 20, "female", 10000, 1),
+//                new Employee(2, "Leo", 20, "male", 10000, 1),
+//                new Employee(3, "Jay", 20, "male", 10000, 1),
+//                new Employee(4, "Wesley", 20, "male", 10000, 2),
+//                new Employee(5, "Andy", 20, "male", 10000, 2)));
+
+        employeeRepository.deleteAll();
+        employeeRepository.resetAutoIncrement();
+        employeeRepository.save(new Employee(1, 20, null, "female", "Hilary", 10000));
+        employeeRepository.save(new Employee(2, 20, null, "male", "Leo", 10000));
+        employeeRepository.save(new Employee(3, 20, null, "male", "Jay", 10000));
+        employeeRepository.save(new Employee(4,20 , null, "male", "Wesley", 10000));
+        employeeRepository.save(new Employee(5, 20, null, "male", "Andy", 10000));
     }
 
 
     @Test
-    public void should_find_employee_by_id(){
+    public void should_return_employee_when_insert_id() {
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/employees/1");
 
         Assert.assertEquals(200, response.getStatusCode());
 
-//        Employee employee = response.getBody().as(Employee.class);
-//        Assert.assertEquals(2, employee.getId().intValue());
-//        Assert.assertEquals("Jay", employee.getName());
+        Employee employee = response.getBody().as(Employee.class);
+        Assert.assertEquals(1, employee.getId().intValue());
+        Assert.assertEquals("Hilary", employee.getName());
     }
 
     @Test
@@ -76,7 +88,7 @@ public class EmployeeControllerTest {
 
     @Test
     public void should_add_employee_successful_when_create_new_employee() {
-        Employee employee = new Employee(5,"Kathy",26,"female",10000,1);
+        Employee employee = new Employee(5, 20, null, "female", "Kathy", 10000);
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .body(employee)
                 .when()
@@ -90,7 +102,7 @@ public class EmployeeControllerTest {
     }
 
     @Test
-    public void should_return_all_employees(){
+    public void should_return_all_employees() {
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/employees");
@@ -112,19 +124,19 @@ public class EmployeeControllerTest {
     }
 
     @Test
-    public void should_delete_employees_successful_when_import_employee_id(){
+    public void should_delete_employees_successful_when_import_employee_id() {
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .delete("/employees/5");
 
         Assert.assertEquals(200, response.getStatusCode());
-       // Assert.assertEquals(3, this.employeeRepository.findAll().size());
+        // Assert.assertEquals(3, this.employeeRepository.findAll().size());
 
     }
 
     @Test
-    public void should_update_employee_information_by_id(){
-        Employee newEmployee = new Employee(3,"Kathy",26,"female",10000,1);
+    public void should_update_employee_information_by_id() {
+        Employee newEmployee = new Employee(3, 20, null, "female", "Kathy", 10000);
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .body(newEmployee)
                 .when()
@@ -137,7 +149,7 @@ public class EmployeeControllerTest {
     }
 
     @Test
-    public void should_return_employees_in_page(){
+    public void should_return_employees_in_page() {
         MockMvcResponse response = given().contentType(ContentType.JSON)
                 .when()
                 .get("/employees?page=2&pageSize=2");
