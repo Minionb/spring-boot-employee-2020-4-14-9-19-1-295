f06b7a3aa8105a92726d9fe44be6552165796da2
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-20 17:21:37 +0800

    Hilary: Done Flyway create db

---
+++ src/main/resources/db/migration/V202004201203__Add_test_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test_table(
+    id Integer not null,
+    name varchar(30) not null
+)
---
+++ src/main/resources/db/migration/V202004201610__Add_employee_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE EMPLOYEE (
+ id INTEGER not null,
+ name VARCHAR(30) not null,
+ age INTEGER not null,
+ gender VARCHAR(30) not null,
+ salary INTEGER not null,
+ companyId INTEGER
+)
